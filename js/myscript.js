18b6cba02e4e3d42e2ab657a84d7ce77fa0bd822
fridalindstrom <frida_lindstrom_@hotmail.com>
2020-11-12 22:36:41 +0000

    feat: adding javascript for new map locations for the city areas on button-clicks

--- js/myscript.js
+++ js/myscript.js
@@ -2,6 +2,34 @@
 
 map = new google.maps.Map(document.getElementById('map'), {
   center: {lat: 59.3293, lng: 18.0686},
-  zoom: 10
+  zoom: 11
 });
+  function newLocation(newLat, newLng) {
+    map.setCenter
+    ({lat: newLat,lng: newLng,})
+  }
+
+
+    $("#sthlm").on("click", function () {
+      newLocation(59.3293, 18.0686),
+      map.setZoom(11);
+       $(".sthlm").css("display" , "block");
+       $(".vasastan").css("display" , "none");
+       $(".norrmalm").css("display" , "none");
+       $(".djurgarden").css("display" , "none");
+       $(".sodermalm").css("display" , "none");
+       $(".ostermalm").css("display" , "none");
+       $(".gamlastan").css("display" , "none");
+    });
+      $("#norrmalm").on("click", function () {
+      newLocation(59.3346, 18.0605),
+      map.setZoom(14);
+       $(".sthlm").css("display" , "none");
+       $(".vasastan").css("display" , "none");
+       $(".norrmalm").css("display" , "block");
+       $(".djurgarden").css("display" , "none");
+       $(".sodermalm").css("display" , "none");
+       $(".ostermalm").css("display" , "none");
+       $(".gamlastan").css("display" , "none");
+    });
 });
