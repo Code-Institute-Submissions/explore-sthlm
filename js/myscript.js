4ec4c70ea4603c1ba8978ebfd07e68566b79cec2
fridalindstrom <frida_lindstrom_@hotmail.com>
2020-11-17 16:33:50 +0000

    style: fixing minor styling in css and html

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,8 @@
-    // the three maps 
+  
+  
+  // the three maps 
+  function initMap() {
+  
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 59.3293, lng: 18.0686 },
     zoom: 11,
@@ -11,8 +15,8 @@
     center: { lat: 59.3293, lng: 18.0686 },
     zoom: 11,
   });
- 
-
+    }
+  $(document).ready(function () {
   function newLocation(newLat, newLng) {
     map.setCenter({ lat: newLat, lng: newLng });
   }
@@ -265,7 +269,8 @@
       $(".cafes").css("display", "none");
       $(".dinner").fadeIn(2000).css("display", "block");
   });
-   $(document).ready(function () {
+
+ 
       $(".burger-menu").click(function(){
       $(".navigation-list").slideToggle(1500);
 });
