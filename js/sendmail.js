4f787976a7001e9840f25023ebae153dc640d6c4
fridalindstrom <frida_lindstrom_@hotmail.com>
2020-11-17 18:16:52 +0000

    fix: emailjs

--- js/sendmail.js
+++ js/sendmail.js
@@ -1,6 +1,6 @@
     window.onload = function() {
-            document.getElementById('contact-form').addEventListener('submit', function() {
-                 location.reload();
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
                
                 emailjs.sendForm('contact_service', 'contact_form', this)
                     .then(function() {
@@ -8,7 +8,6 @@
                     }, function(error) {
                         console.log('FAILED...', error);
                     });
-                   
-                    
+                       
             });
         }
